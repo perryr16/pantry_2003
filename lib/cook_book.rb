7b98b33cae3132fdd25147df1826409b6fdaf697
Ross Perry <perryr16@gmail.com>
2020-04-22 09:51:20 -0600

    returns highest calorie meal

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,19 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredient|
+        ingredients << ingredient[0].name
+      end
+    end
+    ingredients.uniq
+  end
+
+  def highest_calorie_meal
+    @recipes.sort {|recipe| recipe.total_calories}.first
+  end
+
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -20,7 +20,76 @@ class CookBookTest < MiniTest::Test
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 
+  def test_it_returns_all_ingredients
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    assert_equal 440, recipe1.total_calories
+    assert_equal 675, recipe2.total_calories
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, cookbook.ingredients
+  end
+
+  def test_it_returns_highest_calorie_meal
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal recipe2, cookbook.highest_calorie_meal
   end
 
 end
+
+
+
+#
+#
+#
+# # =>
+#
+# # => #<Recipe:0x00007faae692a110...>
+#
+#pantry.restock(ingredient1, 5)
+#
+#pantry.restock(ingredient1, 10)
+#
+#pantry.enough_ingredients_for?(recipe)
+# # => false
+#
+#pantry.restock(ingredient2, 7)
+#
+#pantry.enough_ingredients_for?(recipe)
+# # => false
+#
+#pantry.restock(ingredient2, 1)
+#
+#pantry.enough_ingredients_for?(recipe)
+# # => true
