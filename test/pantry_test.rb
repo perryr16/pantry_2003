1bfd44896484e11e72fdc644b7448d2b5124d4e8
Ross Perry <perryr16@gmail.com>
2020-04-22 09:18:23 -0600

    ingredient 2 stock tested

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -19,32 +19,15 @@ class PantryTest < MiniTest::Test
   def test_it_checks_and_stocks
     pantry = Pantry.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    # ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     assert_equal 0, pantry.stock_check(ingredient1)
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
     assert_equal 15, pantry.stock_check(ingredient1)
 
-
-
+    pantry.restock(ingredient2, 7)
+    assert_equal 7, pantry.stock_check(ingredient2)
   end
 
 
-
 end
-
-#
-# # => #<Ingredient:0x007fd88582ed98...>
-#
-
-#
-# # => 0
-#
-#
-#
-# # => 15
-#
-#pantry.restock(ingredient2, 7)
-#
-#pantry.stock_check(ingredient2)
-# # => 7
