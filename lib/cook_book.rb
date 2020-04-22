620831121101f9fa6f4ed36f2a36e8e998debfc5
Ross Perry <perryr16@gmail.com>
2020-04-22 11:08:09 -0600

    summary functional

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,8 +1,14 @@
+require 'date'
 class CookBook
 
   attr_reader :recipes
   def initialize
     @recipes = []
+    date
+  end
+
+  def date
+    @date = Date.today.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
@@ -21,6 +27,36 @@ class CookBook
 
   def highest_calorie_meal
     @recipes.sort {|recipe| recipe.total_calories}.first
+  end
+
+  def ingredient_details(recipe)
+    ingredient_details = []
+
+    recipe.ingredients_required.each do |ingredient, count|
+      one_ingredient = {}
+      one_ingredient[:ingredient] = ingredient.name
+      one_ingredient[:amount] = "#{count} #{ingredient.unit}"
+      one_ingredient[:total_calories] = ingredient.calories * count
+      ingredient_details << one_ingredient
+    end
+    ingredient_details = ingredient_details.sort_by do |details|
+      details[:total_calories]
+    end.reverse
+    ingredient_details.map { |details| details.delete(:total_calories)}
+    ingredient_details
+  end
+
+
+  def summary
+    summary = Hash.new { |hash, key| hash[key] = {:name => "", :details => {}} }
+    @recipes.each do |recipe|
+
+      summary[recipe.name][:name] = recipe.name
+      summary[recipe.name][:details][:ingredients] = ingredient_details(recipe)
+      summary[recipe.name][:details][:total_calories] = recipe.total_calories
+
+    end
+    summary.values
   end
 
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require 'pry'
 require './lib/ingredient'
 require './lib/pantry'
 require './lib/recipe'
 require './lib/cook_book'
+
 
 class CookBookTest < MiniTest::Test
 
@@ -66,30 +68,53 @@ class CookBookTest < MiniTest::Test
     assert_equal recipe2, cookbook.highest_calorie_meal
   end
 
+  def test_it_returns_date
+    cookbook = CookBook.new
+    # assert_equal "04-22-2020", cookbook.date
+    cookbook.stubs(:date).returns("04-22-2020")
+    assert_equal "04-22-2020", cookbook.date
+  end
+
+  def test_it_returns_summary_by_calories
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new(name: "Cheese", unit: "C", calories: 100)
+    ingredient2 = Ingredient.new(name: "Macaroni", unit: "oz", calories: 30)
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new(name: "Ground Beef", unit: "oz", calories: 100)
+    ingredient4 = Ingredient.new(name: "Bun", unit: "g", calories: 1)
+    recipe2 = Recipe.new("Burger")
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 100)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expected = [{:ingredient=>"Macaroni", :amount=>"8 oz"},
+                {:ingredient=>"Cheese", :amount=>"2 C"}]
+    assert_equal expected, cookbook.ingredient_details(recipe1)
+
+    expected = {:ingredient=>"Ground Beef", :amount=>"4 oz"},
+    {:ingredient=>"Bun", :amount=>"100 g"}
+    assert_equal expected, cookbook.ingredient_details(recipe2)
+
+
+    expected =  [{
+                  :name=>"Mac and Cheese",
+                  :details=>
+                            {:ingredients=>[
+                                            {:ingredient=>"Macaroni", :amount=>"8 oz"},
+                                            {:ingredient=>"Cheese", :amount=>"2 C"}],
+                            :total_calories=>440}},
+
+                  {:name=>"Burger",
+                   :details=>
+                        {:ingredients=>[
+                          {:ingredient=>"Ground Beef", :amount=>"4 oz"},
+                          {:ingredient=>"Bun", :amount=>"100 g"}],
+                    :total_calories=>500}}]
+    assert_equal expected, cookbook.summary
+
+  end
+
 end
-
-
-
-#
-#
-#
-# # =>
-#
-# # => #<Recipe:0x00007faae692a110...>
-#
-#pantry.restock(ingredient1, 5)
-#
-#pantry.restock(ingredient1, 10)
-#
-#pantry.enough_ingredients_for?(recipe)
-# # => false
-#
-#pantry.restock(ingredient2, 7)
-#
-#pantry.enough_ingredients_for?(recipe)
-# # => false
-#
-#pantry.restock(ingredient2, 1)
-#
-#pantry.enough_ingredients_for?(recipe)
-# # => true
